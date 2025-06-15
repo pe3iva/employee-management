5a1311116243a42a819d52a98170a6a8cf2988cc
pe3iva <dawgsofast@protonmail.com>
2025-06-15 21:55:56 +0200

    Add headers and other cpp files for implementation of the features

---
+++ src/goTo.cpp
@@ -0,0 +1,18 @@
+#include "goTo.h"
+
+int goTo(int destination) {
+
+	if (destination == 1) {
+		std::cout << "1";
+	}
+	else if (destination == 2) {
+		std::cout << "2";
+	}
+	else if (destination == 3) {
+		std::cout << "3";
+	}
+	else {
+		std::cout << "4";
+	}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "goTo.h"
 #include <iostream>
 
 int main() {
@@ -22,7 +23,16 @@ int main() {
 
 	} while (password.length() < 5);
 
-	std::cout << "Welcome, " << username << "!\n";
+	std::cout << "\n\n\nWelcome, " << username << "!\n\n";
 
+	std::cout << "Choose where to go: \n\n\n";
+
+	std::cout << "1. Add employee information\n";
+	std::cout << "2. Delete employee information\n";
+	std::cout << "3. Amend employee information\n";
+	
+	int destination{};
+
+	goTo(destination);
 
 }
